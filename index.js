7d60a2036e64b70d12dd3829996fb41765183f17
joelgogo15-ZabuYz <joelgogo15@gmail.com>
2024-02-26 16:51:24 +0000

    completed tests/writing prompts

--- index.js
+++ index.js
@@ -12,4 +12,42 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+async function inquirerteamManager(){
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name:'name',
+        message: 'Please enter the team managers name' 
+    } , 
+    {
+        type: 'input',
+        name:'id',
+        message: 'What is your ID number'
+    }, 
+    {
+        type:'input',
+        name:'email',
+        message:'enter your email address'
+    },
+    {
+        type:'input',
+        name:'officeNumber',
+        message:'What is your office number'
+    }, 
+    {
+        type:'choice'
+    }
+
+])
+}
+
+async function inquirerEngineer(){
+inquirer.prompt([
+    {
+
+    }
+])
+}
+
 
